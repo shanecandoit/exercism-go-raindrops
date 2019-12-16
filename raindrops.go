b5d579e51d3e3670e62a9bd5fb706d461f576b61
shane <shanecandoit@gmail.com>
2019-12-16 11:06:06 -0600

    clean up. add comments.

--- raindrops.go
+++ raindrops.go
@@ -1,25 +1,20 @@
 // Package raindrops is kind of like fizz buzz
 package raindrops
 
-/*
-
-
-*/
-
-import(
+import (
 	"strconv"
 )
 
 // Convert a number to a string
-// The rules of raindrops are that if a given number return a string that
+// given the rules of raindrops: that if a given number return a string that
 func Convert(num int) string {
 
 	res := ""
 	number := strconv.Itoa(num)
 
-	//has 3 as a factor, add 'Pling' to the result.
-//has 5 as a factor, add 'Plang' to the result.
-//has 7 as a factor, add 'Plong' to the result.
+	// has 3 as a factor, add 'Pling' to the result.
+	// has 5 as a factor, add 'Plang' to the result.
+	// has 7 as a factor, add 'Plong' to the result.
 	if num%3 == 0 {
 		res += "Pling"
 	}
@@ -30,8 +25,9 @@ func Convert(num int) string {
 		res += "Plong"
 	}
 
-	//does not have any of 3, 5, or 7 as a factor, the result should be the digits of the number.
-	if len(res)==0 {
+	// does not have any of 3, 5, or 7 as a factor,
+	// the result should be the digits of the number.
+	if len(res) == 0 {
 		res = number
 	}
 
